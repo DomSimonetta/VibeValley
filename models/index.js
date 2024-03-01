8e6c84b8dd006eafc3daecb24e0df040e54eeb02
akent <alexanderkent099@gmail.com>
2024-02-29 20:09:09 -0500

    Establish association: Playlist has many Songs, and Song belongs to a Playlist

--- models/index.js
+++ models/index.js
@@ -3,3 +3,12 @@ const Playlist = require('./playlist');
 const Song = require('./song');
 const Comment = require('./comment');
 const Like = require('./like');
+
+Playlist.hasMany(Song, {
+    foreignKey: 'playlistId',
+    as: 'songs'
+});
+Song.belongsTo(Playlist, {
+    foreignKey: 'playlistId',
+    as: 'playlist'
+});
