653c73e410a935136bf14affd8521b718debcc00
akent <alexanderkent099@gmail.com>
2024-02-29 19:59:07 -0500

    Create Comment model with text field

---
+++ models/comment.js
@@ -0,0 +1,22 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    text: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+}, {
+    sequelize,
+    timestamps: true,
+    modelName: 'Comment'
+});
+
+module.exports = Comment;
