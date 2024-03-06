14412ec690d6dd06feddaf0975792e58f56c579b
akent <alexanderkent099@gmail.com>
2024-03-06 17:05:38 -0500

    Correcting routes between multiple files

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Add routes to the application
-app.use(routes);
+app.use('/api', routes);
 
 // Sync sequelize models to the database, then start the server
 sequelize.sync({ force: false }).then(() => {
