d6265661c713a75999d0b290cb76021dd6dacbe0
Jakob A <jdanders1999@gmail.com>
2024-03-06 20:40:52 -0500

    changed 'comments' to 'comment'

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-// const commentController = require('../commentController');
+const commentController = require('../commentController');
 
 // // POST a comment on a song
-// router.post('/:songId/comments', commentController.postComment);
+router.post('/:songId/comment', commentController.postComment);
 
-// // GET comments on a song
-// router.get('/:songId/comments', commentController.getCommentsOnSong);
+// GET comments on a song
+router.get('/:songId/comment', commentController.getCommentOnSong);
 
 module.exports = router;
